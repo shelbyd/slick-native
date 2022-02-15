b75844697ac54de1b85bdc307c68e70fc5320534
Shelby Doolittle <shelby@shelbyd.com>
2022-02-15 00:44:40 -0500

    add react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
+import { Provider as PaperProvider } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
