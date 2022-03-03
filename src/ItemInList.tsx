aa45c23e6048b6330d56ab007faf63af95289292
Shelby Doolittle <shelby@shelbyd.com>
2022-03-03 18:25:24 -0500

    navigate to item unless another press is provided

--- src/ItemInList.tsx
+++ src/ItemInList.tsx
@@ -1,15 +1,21 @@
 import { List } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 import { Item, KIND_DATA } from './Item';
 
 export function ItemInList({ item, onPress }: { item: Item, onPress: () => void }) {
   const kindDesc = KIND_DATA[item.kind];
+  const navigation = useNavigation();
+
+  const navigateToItem = () => {
+    navigation.push('ItemDetails', {item: item});
+  };
 
   return (
     <List.Item
         title={item.title}
         key={item.id}
         left={props => <List.Icon {...props} color={kindDesc.color} icon={kindDesc.icon} />}
-        onPress={onPress} />
+        onPress={onPress || navigateToItem} />
   );
 }
