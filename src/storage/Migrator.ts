3db1d19a16062e78f79095758c388af674c7348c
Shelby Doolittle <shelby@shelbyd.com>
2022-03-23 13:25:58 -0400

    implement new migration and restore from snapshot

--- src/storage/Migrator.ts
+++ src/storage/Migrator.ts
@@ -7,14 +7,36 @@ export class Migrator {
   constructor(private readonly storage: AsyncStorage) {}
 
   async perform() {
-    let version = await this.storage.getItem('@storage-meta/version');
-    if (version == null) {
-      await storeSnapshot(this.storage);
-      await parentChildDag(this.storage);
-      version = 1;
-      await this.storage.setItem('@storage-meta/version',
-                                 JSON.stringify(version));
+    let version = await this.getVersion();
+
+    if (version == 0) {
+      // Save relationships between items in DagStorage.
+      version = await this.incrementVersion(() => resaveAllItems(this.storage));
     }
+
+    if (version == 1) {
+      // Start saving ids for open items in a separate set.
+      version = await this.incrementVersion(() => resaveAllItems(this.storage));
+    }
+  }
+
+  private async getVersion() {
+    return JSON.parse(await this.storage.getItem('@storage-meta/version') ||
+                      '0');
+  }
+
+  private async setVersion(v: number) {
+    await this.storage.setItem('@storage-meta/version', JSON.stringify(v));
+  }
+
+  async incrementVersion(cb: () => Promise<void>) {
+    await storeSnapshot(this.storage);
+
+    await cb();
+
+    const version = await this.getVersion() + 1;
+    await this.setVersion(version);
+    return version;
   }
 }
 
@@ -25,7 +47,21 @@ async function storeSnapshot(storage: AsyncStorage) {
   await storage.setItem('@storage-meta/backup', JSON.stringify(items));
 }
 
-async function parentChildDag(storage: AsyncStorage) {
+async function restoreFromSnapshot(storage: AsyncStorage) {
+  const keys =
+      (await storage.getAllKeys()).filter(k => !k.startsWith('@storage-meta'));
+  await Promise.all(keys.map(k => storage.removeItem(k)));
+
+  let json = JSON.parse(await storage.getItem('@storage-meta/backup'));
+  for (let [key, value] of json) {
+    if (typeof value !== 'string') {
+      value = JSON.stringify(value);
+    }
+    await storage.setItem(key, value);
+  }
+}
+
+async function resaveAllItems(storage: AsyncStorage) {
   const store = new Store(storage);
 
   const keys = await storage.getAllKeys();
